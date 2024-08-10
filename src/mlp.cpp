da1d2ded704516ceb8265dc8e995e89cf41b54b1
Qwertst <aydreq@mail.ru>
2024-08-10 23:58:40 +0300

    mlp draft

---
+++ src/mlp.cpp
@@ -0,0 +1,55 @@
+#include "mlp.hpp"
+#include <algorithm>
+#include "value.hpp"
+
+namespace nn {
+
+    Neuron::Neuron(size_t in_size, bool bias, bool nonlin): in_size_(in_size), nonlin_(nonlin) {
+        if (bias) {
+            bias_ = make_value();
+        }
+        weights_.reserve(in_size);
+        for (size_t it = 0; it < in_size; ++it) {
+            weights_.push_back(make_value());
+        }
+    }
+
+    Value Neuron::operator()(const std::vector<Value>& input) const {
+        Value res = bias_;
+        std::for_each(weights_.begin(), weights_.end(),[&res](const Value& w){
+            res = res + w;
+        });
+        return nonlin_ ? relu(res) : res;
+    }
+
+    Layer::Layer(size_t in_size, size_t out_size, bool bias, bool nonlin): in_size_(in_size), out_size_(out_size) {
+        neurons_.resize(out_size_);
+        for (size_t it = 0; it < out_size; ++it) {
+            neurons_.emplace_back(in_size, bias, nonlin);
+        }
+    }
+
+    std::vector<Value> Layer::operator()(const std::vector<Value>& input) const {
+        std::vector<Value> res;
+        for (const Neuron& neuron : neurons_) {
+            res.push_back(neuron(input));
+        }
+        return res;
+    }
+
+    MLP::MLP(size_t in_size, std::vector<size_t> out_sizes): in_size_(in_size), out_sizes_(std::move(out_sizes)), n_layers_(out_sizes_.size()) {
+        layers_.emplace_back(in_size, out_sizes_[0]);
+        for (size_t it = 1; it < n_layers_; ++it) {
+            layers_.emplace_back(out_sizes_[it-1], out_sizes_[it], true, it != n_layers_-1);
+        }
+    }
+
+    std::vector<Value> MLP::operator()(const std::vector<Value>& input) const {
+        std::vector<Value> res = input;
+        for (const Layer& layer : layers_) {
+            res = layer(res);
+        }
+        return res;
+    }
+
+}
---
+++ src/mlp.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "value.hpp"
+#include <vector>
+
+namespace nn {
+
+class Neuron {
+public:
+    Neuron(size_t in_size, bool bias = true, bool nonlin = true);
+    Value operator()(const std::vector<Value>& input) const;
+private:
+    std::vector<Value> weights_;
+    Value bias_;
+    size_t in_size_;
+    bool nonlin_;
+};
+
+class Layer {
+public:
+    Layer(size_t in_size, size_t out_size, bool bias = true, bool nonlin = true);
+    std::vector<Value> operator()(const std::vector<Value>& input) const;
+private:
+    std::vector<Neuron> neurons_;
+    size_t in_size_;
+    size_t out_size_;
+};
+
+class MLP {
+public:
+    MLP(size_t in_size, std::vector<size_t> out_sizes);
+    std::vector<Value> operator()(const std::vector<Value>& input) const;
+private:
+    std::vector<Layer> layers_;
+    size_t in_size_;
+    std::vector<size_t> out_sizes_;
+    size_t n_layers_;
+};
+
+} // namespace nn
