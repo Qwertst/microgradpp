64e966e7348bc91f8cba30a730d2c1fdaa7f2741
Qwertst <aydreq@mail.ru>
2024-08-10 20:39:30 +0300

    add implicit casts in operations, clang-formatted

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
 #include "value.hpp"
+#include <iostream>
 
 using namespace nn;
 
 int main() {
-    Value a = make_value(1);
-    Value e = exp(a)+make_value(1);
-    backward(e);
-    std::cout << e << a << '\n';
+  Value a = make_value(1);
+  Value e = (exp(2 * a) - 1) / (exp(2 * a) + 1);
+  backward(e);
+  std::cout << e << a << '\n';
 }
--- src/value.hpp
+++ src/value.hpp
@@ -1,45 +1,57 @@
 #pragma once
 
-#include <vector>
-#include <memory>
 #include <functional>
+#include <memory>
 #include <unordered_set>
+#include <vector>
 
 namespace nn {
 
 class Value_handler;
 using Value = std::shared_ptr<Value_handler>;
 Value make_value(double data, std::vector<Value> childs = {});
-void backward(const Value& value);
+void backward(const Value &value);
 
 class Value_handler {
 public:
-    Value_handler(double data, std::vector<Value> childs);
-    double get_data() const;
-    double get_grad() const;
-    void zero_grad();
+  Value_handler(double data, std::vector<Value> childs);
+  double get_data() const;
+  double get_grad() const;
+  void zero_grad();
 
-    friend void backward(const Value& value);
-    friend Value operator+(const Value& lhs, const Value &rhs);
-    friend Value operator-(const Value& lhs, const Value &rhs);
-    friend Value operator*(const Value& lhs, const Value &rhs);
-    friend Value operator/(const Value& lhs, const Value &rhs);
-    friend Value operator-(const Value& arg);
+  friend void backward(const Value &value);
+  friend Value operator+(const Value &lhs, const Value &rhs);
+  friend Value operator-(const Value &lhs, const Value &rhs);
+  friend Value operator*(const Value &lhs, const Value &rhs);
+  friend Value operator/(const Value &lhs, const Value &rhs);
 
-    friend Value pow(const Value& arg, double k);
-    friend Value relu(const Value& arg);
-    friend Value exp(const Value& arg);
+  friend Value operator+(double lhs, const Value &rhs);
+  friend Value operator-(double lhs, const Value &rhs);
+  friend Value operator*(double lhs, const Value &rhs);
+  friend Value operator/(double lhs, const Value &rhs);
+  friend Value operator+(const Value &lhs, double rhs);
+  friend Value operator-(const Value &lhs, double rhs);
+  friend Value operator*(const Value &lhs, double rhs);
+  friend Value operator/(const Value &lhs, double rhs);
 
-    friend std::ostream& operator<<(std::ostream& os, const Value& value);
+  friend Value operator-(const Value &arg);
 
-    friend Value;
+  friend Value pow(const Value &arg, double k);
+  friend Value relu(const Value &arg);
+  friend Value exp(const Value &arg);
+
+  friend std::ostream &operator<<(std::ostream &os, const Value &value);
+
+  friend Value;
+
 private:
-    double data_;
-    double grad_;
-    std::vector<Value> prev_;
-    std::function<void()> backward_;
+  double data_;
+  double grad_;
+  std::vector<Value> prev_;
+  std::function<void()> backward_;
 
-    static void top_sort(const Value& value, std::unordered_set<Value>& visited, std::vector<Value>& order);
+  static void top_sort(const Value &value, std::unordered_set<Value> &visited,
+                       std::vector<Value> &order);
 };
 
-}
+} // namespace nn
